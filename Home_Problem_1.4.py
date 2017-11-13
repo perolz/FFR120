a61e23aebf2212605a2ead85b5a8f79063cece91
Per <per.olzon@gmail.com>
2017-11-13 15:45:41 +0100

    All Done

--- Home_Problem_1.4.py
+++ Home_Problem_1.4.py
@@ -13,9 +13,9 @@ if __name__=="__main__":
     #transmissionRateSettings = []
     correspondingRecovered = []
     #initialSpread=[]
-    numberOfIndexes=3
+    numberOfIndexes=15
     transmissionRateSettings=np.linspace(0.1,0.5,numberOfIndexes)
-    initialSpread=np.linspace(10,210,numberOfIndexes)
+    initialSpread=np.linspace(10,150,numberOfIndexes)
     xv,yv=np.meshgrid(initialSpread,transmissionRateSettings)
     correspondingRecovered=np.empty([numberOfIndexes,numberOfIndexes])
     for k in range(len(xv)):
@@ -28,7 +28,7 @@ if __name__=="__main__":
 
             averageRecovered = []
 
-            for j in range(20):
+            for j in range(10):
                 while len(infected[0])>0:
                     (infected, susceptibles, recovered) = diseaseSpreading(infected, susceptibles, recovered, yv[k][i],
                                                                            recoveryRate)
@@ -50,11 +50,12 @@ if __name__=="__main__":
     # plt.plot([0.05,0.5],[Rinf,Rinf],'r')
     # plt.text(0.01,Rinf,r'$R_\infty$ = %1.1f' % Rinf)
 
-    plt.xlabel('initialSpread',fontsize=14)
-    plt.ylabel('transmissionRateSettings',fontsize=14)
-    ax.zlabel('correspondingRecovered',fontsize=14)
+    ax.set_xlabel('initialSpread',fontsize=14)
+    ax.set_ylabel('transmissionRateSettings',fontsize=14)
+    ax.set_zlabel('correspondingRecovered',fontsize=14)
     # plt.title(r'Test with different values of recovery rates $\beta$=%1.1f' %transmissionRate)
     #plt.legend(handles=[h1], loc=1)
-    #fig.savefig('Plotfor1.3_Beta%1.1f.png' %transmissionRate)
+    fig.savefig('Plotfor1.4_Beta%1.1f.png' %transmissionRate)
     plt.show()
-
+    with open('1.4Data.txt','w') as f:
+        f.write(str(xv) + '\n\n' +str(yv) + '\n\n' +str(correspondingRecovered) + '\n\n' )
